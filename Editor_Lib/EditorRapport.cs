14a7f75d128f1e207d37cd9aacd3638f1f488ac8
rpriisholm <rpriisholm@hotmail.com>
2017-02-25 14:50:05 +0100

    Upd1

---
+++ Editor_Lib/EditorRapport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editor_Lib
+{
+    public static class EditorRapport
+    {
+        public static void GenerateAnonymousRapport(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+        }
+
+        public static List<String> FilterStrings(List<string> strings, string startsWith, string endsWith)
+        {
+            List<string> filteredStrings = new List<string>();
+
+            foreach(string value in strings)
+            {
+                if(value.StartsWith && value.EndsWith)
+                {
+
+                }
+            }
+        }
+    }
+}
